81b10142131b157d005d09f1841b4f61cb94bba3
chadgarlandscg <chadgarlandscg@gmail.com>
2025-04-16 01:05:05 -0600

    Fix DB name

--- iac/sql-db/main.tf
+++ iac/sql-db/main.tf
@@ -1,10 +1,10 @@
-locals {
-  application_name     = "GranuleService"
+locals {  # need to get this from same place as bounded-context
+  bounded_context_name = "GranuleContext"
 }
 
 module "granular_bounded_context_sql_db" {
   source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/sql-database/aws?ref=main"
-  database_name = "${var.bounded_context_name}Db"
+  database_name = "${local.bounded_context_name}Db"
   vpc = var.vpc
   application_security_groups = [var.application_security_group_id]
 }
